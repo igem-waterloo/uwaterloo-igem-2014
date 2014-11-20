1872c11b1ff56e31b6d8d37ee8071fa79adba07c
jrhawley <jamesrichardhawley@gmail.com>
2014-11-20 18:13:26 -0500

    Changed sign on d_ms

--- RNAi/RNAi_DE_System.m
+++ RNAi/RNAi_DE_System.m
@@ -44,7 +44,7 @@ function dS = RNAi_DE_System( ~, S, p )
     d_H =  alpha_H*h       -beta_H*H       -k_1*H*s +k_m1*Hs  +k_m3*Hms;
     d_Hs =                 -beta_H*Hs      +k_1*H*s -k_m1*Hs  -k_2*Hs*m;
     d_Hms=                 -beta_H*Hms                        +k_2*Hs*m -k_m3*Hms;
-    d_ms = k_4*m*s         -beta_ms*ms                        -k_m3*Hms;
+    d_ms = k_4*m*s         -beta_ms*ms                        +k_m3*Hms;
     
 	dS=[d_s; d_m; d_M; d_h; d_H; d_Hs; d_Hms; d_ms]; 
 end
